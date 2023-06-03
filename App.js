15663e63aa33f351143de08eccbc95fa24e0d509
Vivek Bhookya <v.bhookya98@gmail.com>
2023-06-03 15:50:43 -0500

    Added two assessments to get started

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Linking, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>MentalHealth</Text>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={styles.hyperlinkStyle}
+        onPress={() => {
+          Linking.openURL('https://psychologytoday.tests.psychtests.com/bin/transfer');
+        }}>
+        Open Mental Health Assessment
+      </Text>
+      <Text
+        style={styles.hyperlinkStyle}
+        onPress={() => {
+          Linking.openURL('https://www.mdcalc.com/calc/1727/gad7-general-anxiety-disorder7');
+        }}>
+        Open General Anxiety Disorder Assessment
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +28,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  hyperlinkStyle: {
+    color: 'blue',
   },
 });
