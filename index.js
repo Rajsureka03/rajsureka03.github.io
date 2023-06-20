d5d23cf9fcd33c68716fbd1be0ad61e4d573fc5a
Raj Sureka <Rajsureka03@gmail.com>
2023-06-20 19:10:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -242,14 +242,12 @@ function NextQuestion(index) {
 
 function checkForAnswer() {
     const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
-    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const currentQuestionAnswer = currentQuestion //gets current Question's answer
     const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
-    let correctOption = null
 
     options.forEach((option) => {
         if (option.value === currentQuestionAnswer) {
             //get's correct's radio input with correct answer
-            correctOption = option.labels[0].id
         }
     })
 
@@ -263,11 +261,11 @@ function checkForAnswer() {
     options.forEach((option) => {
         if (option.checked === true && option.value === currentQuestionAnswer) {
             //document.getElementById(correctOption).style.backgroundColor = "green"
-            //playerScore++ //adding to player's score
+            playerScore++ //adding to player's score
             indexNumber++ //adding 1 to index so has to display next question..
             //set to delay question number till when next question loads
             if (option[0].checked === true && option[1].checked === false && option[2].checked === false && option[3].checked == false) {
-                playerScore++
+                playerScore+1
             }
             setTimeout(() => {
                 questionNumber++
@@ -276,7 +274,7 @@ function checkForAnswer() {
         }
 
         else if (option.checked && option.value !== currentQuestionAnswer) {
-            const wrongLabelId = option.labels[0].id
+            //const wrongLabelId = option.labels[0].id
             //document.getElementById(wrongLabelId).style.backgroundColor = "red"
             //document.getElementById(correctOption).style.backgroundColor = "green"
             //wrongAttempt++ //adds 1 to wrong attempts 
